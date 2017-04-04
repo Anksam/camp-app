a15a3836fe65f8c7741c4316d5ff90d163f548e3
Ankesh Samarth <aankeshsamarth@gmail.com>
2017-04-04 15:54:42 +0530

    Added test data

--- app/documents/document.js
+++ app/documents/document.js
@@ -1,7 +1,2 @@
-export interface Document {
-  title: string,
-  description: string,
-  file_url: string,
-  updated_at: string,
-  image_url: string,
-}
+"use strict";
+//# sourceMappingURL=document.js.map
